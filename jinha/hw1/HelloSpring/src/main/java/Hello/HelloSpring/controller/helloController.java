bd24fff46b4a2b4879c5ee7d03d2994533a53225
Jinha Kim <kjh03124@naver.com>
2024-03-31 23:45:27 +0900

    ⚡️ Section 2-3 : API 개발 방식
    API 개발 방식의 컨트롤러 코드 추가

--- jinha/hw1/HelloSpring/src/main/java/Hello/HelloSpring/controller/helloController.java
+++ jinha/hw1/HelloSpring/src/main/java/Hello/HelloSpring/controller/helloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class helloController {
@@ -25,5 +26,35 @@ public class helloController {
         model.addAttribute("name", name);
         //외부에서 data를 전달받아 Model 객체에 저장 후 View page를 반환
         return "hello-template";
+    }
+
+    //그대로 data를 넘겨줌
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name){
+        return "hello " + name;
+    }
+
+    //json 형식으로 값을 전달해주는 방식_ key, value로 넘어간다.
+    //localhost:8080/hello-api?name=jinha 라는 url이 넘어오면 key값은 name, value값은 jinha
+    //가장 많이 사용하는 방식_ 객체를 반환하여 json으로 변환
+    @GetMapping("hello-api")
+    @ResponseBody //HTTP Body에 문자 내용을 직접 반환해줌
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello{
+        private String name;
+
+        public String getName(){
+            return name;
+        }
+
+        public void setName(String name){
+            this.name = name;
+        }
     }
 }
