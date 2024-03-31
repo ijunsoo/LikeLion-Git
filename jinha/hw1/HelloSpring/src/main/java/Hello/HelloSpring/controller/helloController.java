5a0c7fbc994864c8a7f0bb2982f8ed025d3257b4
Jinha Kim <kjh03124@naver.com>
2024-03-31 23:25:30 +0900

    💄 Section 2-2 : MVC와 템플릿 엔진
    Model과 View를 이용한 개발 코드 추가

--- jinha/hw1/HelloSpring/src/main/java/Hello/HelloSpring/controller/helloController.java
+++ jinha/hw1/HelloSpring/src/main/java/Hello/HelloSpring/controller/helloController.java
@@ -3,6 +3,7 @@ package Hello.HelloSpring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class helloController {
@@ -15,5 +16,14 @@ public class helloController {
         return "hello";
         //view page인 hello.html을 찾아서 반환해줌.
         //viewResolver에 의해 템플릿을 찾아가게 된다.
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(value = "name") String name, Model model){
+        //RequestParam nullable 여부의 Default는 true이다.
+        //http method get으로 localhost:8080/hello-mvc?name={name}의 url이 넘어온다.
+        model.addAttribute("name", name);
+        //외부에서 data를 전달받아 Model 객체에 저장 후 View page를 반환
+        return "hello-template";
     }
 }
