b436820dba84c52f1a12b79a32137f97dc504974
Jinha Kim <kjh03124@naver.com>
2024-04-02 00:52:58 +0900

    ✨ Section 5-3 : 회원 웹 기능_조회
    회원 웹 기능 회원 조회 기능을 위한 컨트롤러 코드 추가 및  html 파일 추가

--- jinha/hw1/HelloSpring/src/main/java/Hello/HelloSpring/controller/MemberController.java
+++ jinha/hw1/HelloSpring/src/main/java/Hello/HelloSpring/controller/MemberController.java
@@ -4,8 +4,11 @@ import Hello.HelloSpring.domain.Member;
 import Hello.HelloSpring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -40,5 +43,13 @@ public class MemberController {
         System.out.println("member = "+member.getName());
 
         return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model){
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+
+        return "members/memberList";
     }
 }
