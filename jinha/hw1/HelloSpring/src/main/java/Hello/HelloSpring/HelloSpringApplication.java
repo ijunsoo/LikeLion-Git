86aa1e565b1aeb8a64e6d81e1b95b6ab09781270
Jinha Kim <kjh03124@naver.com>
2024-04-01 23:39:21 +0900

    ✨ Section 4-1 : 컴포넌트 스캔과 자동 의존관계 설정
    MemberController 추가 및 컴포넌트 스캔을 위한 어노테이션 추가

--- jinha/hw1/HelloSpring/src/main/java/Hello/HelloSpring/HelloSpringApplication.java
+++ jinha/hw1/HelloSpring/src/main/java/Hello/HelloSpring/HelloSpringApplication.java
@@ -1,4 +1,5 @@
 package Hello.HelloSpring;
+//spring container는 위의 package와 동일 및 하위 패키지만 컴포넌트 스캔을 통해 빈으로 등록한다.
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- jinha/hw1/HelloSpring/src/main/java/Hello/HelloSpring/service/MemberService.java
+++ jinha/hw1/HelloSpring/src/main/java/Hello/HelloSpring/service/MemberService.java
@@ -3,14 +3,18 @@ package Hello.HelloSpring.service;
 import Hello.HelloSpring.domain.Member;
 import Hello.HelloSpring.repository.MemberRepository;
 import Hello.HelloSpring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service //for component scan
 //메서드 이름을 지정할 때 service는 보통 business에 더 의존적으로 설계를 하고, repository는 기계적인 용어들을 선택한다.
 public class MemberService {
     private final MemberRepository memberRepository;
 
+    @Autowired
     public MemberService(MemberRepository memberRepository){
         this.memberRepository = memberRepository;
     }
