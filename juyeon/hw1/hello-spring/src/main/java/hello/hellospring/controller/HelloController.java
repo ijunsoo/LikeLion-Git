18301c2ff37a5c1deaca1feae1ba48cbf788328b
JuyeonLee <dlwndus708@naver.com>
2024-04-02 19:42:56 +0900

    Section 3-2 : 회원 도메인과 리포지토리 만들기

--- juyeon/hw1/hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ juyeon/hw1/hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -32,11 +32,12 @@ public class HelloController{
 		Hello hello = new Hello();
 		hello.setName(name);
 		return hello;
+		
 	}
 
 	static class Hello {
 		private String name;
-		
+	
 		public String getName()
 		{
 			return name;
