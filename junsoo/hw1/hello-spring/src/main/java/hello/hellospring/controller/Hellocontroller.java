57695abb8bd24600d2842645424f9b28c9a0d6ae
ijunsoo <159103870+ijunsoo@users.noreply.github.com>
2024-04-03 22:01:26 +0900

    section2-2: mvc 와템플릿 엔진

--- junsoo/hw1/hello-spring/src/main/java/hello/hellospring/controller/Hellocontroller.java
+++ junsoo/hw1/hello-spring/src/main/java/hello/hellospring/controller/Hellocontroller.java
@@ -2,6 +2,7 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class Hellocontroller {
@@ -9,5 +10,11 @@ public class Hellocontroller {
     public String hello(Model model){
         model.addAttribute("data","hello!!");
         return "hello";
+    }
+    @GetMapping("hello-mvc")
+    public String hellomvc(@RequestParam(value="name",required = true)String name,Model model){
+        model.addAttribute("name",name);
+        return "hello-template";
+
     }
 }
