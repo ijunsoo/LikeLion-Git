580c6925d0204772d65b98523fe7246a77733a07
ijunsoo <159103870+ijunsoo@users.noreply.github.com>
2024-04-03 23:46:58 +0900

    section 3-4:회원 서비스

---
+++ junsoo/hw1/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -0,0 +1,30 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    public Long join(Member member){
+        //중복회원 안됨
+        memberRepository.findByname(member.getName())
+                    .ifPresent(m ->{
+                        throw new IllegalStateException("이미 존재하는 회원입니다");
+                    } );
+        memberRepository.save(member);
+        return member.getId();
+    }
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findById(memberId);
+
+    }
+}
