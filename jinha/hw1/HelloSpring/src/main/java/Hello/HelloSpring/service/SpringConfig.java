8bc380c19697a1886b855aede238f5bcc59fdef7
Jinha Kim <kjh03124@naver.com>
2024-04-04 02:43:55 +0900

    ✨ Section 6-2 : 순수 JDBC
    순수 JDBC 개발을 위한 build.gradle, application.properties 파일 수정, JdbcMemoryRepository 파일 추가, SpringConfig 파일 수정

--- jinha/hw1/HelloSpring/src/main/java/Hello/HelloSpring/service/SpringConfig.java
+++ jinha/hw1/HelloSpring/src/main/java/Hello/HelloSpring/service/SpringConfig.java
@@ -1,12 +1,20 @@
 package Hello.HelloSpring.service;
 
+import Hello.HelloSpring.repository.JdbcMemberRepository;
 import Hello.HelloSpring.repository.MemberRepository;
-import Hello.HelloSpring.repository.MemoryMemberRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+    //@Autowired private DataSource dataSource;혹은
+    private DataSource dataSource; //이렇게도 가능하다.
+
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService(){
@@ -15,6 +23,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+        //return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
