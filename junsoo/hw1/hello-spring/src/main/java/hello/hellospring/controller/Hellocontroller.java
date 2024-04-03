9c7a6ec7c77630ae28bea1eef589a98480114a72
ijunsoo <159103870+ijunsoo@users.noreply.github.com>
2024-04-04 01:30:59 +0900

    section 5-2: 회원 웹기능 - 등록

--- junsoo/hw1/hello-spring/src/main/java/hello/hellospring/controller/Hellocontroller.java
+++ junsoo/hw1/hello-spring/src/main/java/hello/hellospring/controller/Hellocontroller.java
@@ -1,12 +1,14 @@
 package hello.hellospring.controller;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller
+
 public class Hellocontroller {
+
     @GetMapping("hello")
     public String hello(Model model){
         model.addAttribute("data","hello!!");
