07894c397b131a95614f5143d18dd45454bc3e3f
ijunsoo <159103870+ijunsoo@users.noreply.github.com>
2024-04-03 22:26:09 +0900

    section2-3: API

--- junsoo/hw1/hello-spring/src/main/java/hello/hellospring/controller/Hellocontroller.java
+++ junsoo/hw1/hello-spring/src/main/java/hello/hellospring/controller/Hellocontroller.java
@@ -3,6 +3,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class Hellocontroller {
@@ -16,5 +17,27 @@ public class Hellocontroller {
         model.addAttribute("name",name);
         return "hello-template";
 
+    }
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String hellostring(@RequestParam("name")String name){
+        return "hello " +name;
+    }
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name")String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+    static class Hello{
+        private String name;
+        public String getName(){
+            return name;
+        }
+        public void setName(String name){
+            this.name=name;
+
+        }
     }
 }
