67a7a92114e9f765b4b9bc483bcedb0c448047cf
JuyeonLee <dlwndus708@naver.com>
2024-04-04 22:36:42 +0900

    Section 6-6 : 스프링 데이터 JPA

--- juyeon/hw1/hello-spring/src/main/java/hello/hellospring/service/SpringConfig.java
+++ juyeon/hw1/hello-spring/src/main/java/hello/hellospring/service/SpringConfig.java
@@ -5,16 +5,18 @@ import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import javax.persistence.EntityManager;
+import jakarta.persistence.EntityManager;
+
 import javax.sql.DataSource;
 @Configuration
 public class SpringConfig {
 	
+	private final MemberRepository memberRepository;
 	private EntityManager em;
 	
 	@Autowired
-	public SpringConfig(EntityManager em) {
-		this.em = em;
+	public SpringConfig(MemberRepository memberRepository) {
+		this.memberRepository = memberRepository;
 	}
 	@Bean
 	public MemberService memberService() {
