d191d0b5e3479a9d3e320fc5567afaae26943443
JuyeonLee <dlwndus708@naver.com>
2024-04-04 19:41:14 +0900

    Section 5-1 : 회원 웹 기능- 홈 화면 추가

--- juyeon/hw1/hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
+++ juyeon/hw1/hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
@@ -7,11 +7,10 @@ import hello.hellospring.service.MemberService;
 
 @Controller
 public class MemberController{
-	private final MemberService memberService;
+	private final MemberService memberService; 
 	
 	@Autowired
 	public MemberController(MemberService memberService) {
 		this.memberService = memberService;
-	}
-	
+	}	
 }
