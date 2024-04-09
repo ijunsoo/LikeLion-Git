8fc3a5d0d3aa68bb9e74b8c2a8dcba21e0c46576
Jinha Kim <kjh03124@naver.com>
2024-04-10 00:59:01 +0900

    ✨ Feat. 2-2 : DIP 위반에 따른 코드 수정
    DIP를 충족하기 위한 MemberServiceImpl 코드 의존 관계 수정 및 이에 따른 코드 수정, 사용자의 요구사항 변경에 따른 RateDiscountPolicy 파일 추가, IoC container AppConfig 파일 추가

---
+++ jinha/hw2/spring-core-basic/src/main/java/spring/springcorebasic/AppConfig.java
@@ -0,0 +1,47 @@
+package spring.springcorebasic;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import spring.springcorebasic.discount.DiscountPolicy;
+import spring.springcorebasic.discount.FixDiscountPolicy;
+import spring.springcorebasic.discount.RateDiscountPolicy;
+import spring.springcorebasic.member.MemberRepository;
+import spring.springcorebasic.member.MemberService;
+import spring.springcorebasic.member.MemberServiceImpl;
+import spring.springcorebasic.member.MemoryMemberRepository;
+import spring.springcorebasic.order.OrderService;
+import spring.springcorebasic.order.OrderServiceImpl;
+
+// 공연기획자 파일. Application에 필요한 구현 객체를 생성한다.
+// 설계 변경으로 MemberServiceImpl은 MemoryRepository를 직접 의존하지 않고, 단지 MemberRepository만 의존한다.
+// AppConfig처럼 객체를 생성하고 관리하면서 의존관계를 연결해주는 것을 IoC 컨테이너 혹은 DI 컨테이너라고 한다.
+@Configuration
+public class AppConfig {
+    /*
+    public MemberService memberService(){
+        return new MemberServiceImpl(new MemoryMemberRepository());
+    }
+
+    public OrderService orderService(){
+        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+    }
+
+    // 위와 같이 하면 매개변수에 중복 코드가 발생한다! 그러므로 아래와 같이 메소드로 빼주어야 한다.
+    */
+    @Bean
+    public MemberService memberService(){
+        return new MemberServiceImpl(memberRepository());
+    }
+    @Bean
+    public OrderService orderService(){
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+    @Bean
+    public MemberRepository memberRepository(){
+        return new MemoryMemberRepository();
+    }
+    @Bean
+    public DiscountPolicy discountPolicy(){
+        return new RateDiscountPolicy();
+    }
+}
--- jinha/hw2/spring-core-basic/src/main/java/spring/springcorebasic/order/OrderServiceImpl.java
+++ jinha/hw2/spring-core-basic/src/main/java/spring/springcorebasic/order/OrderServiceImpl.java
@@ -1,16 +1,28 @@
 package spring.springcorebasic.order;
 
 import spring.springcorebasic.discount.DiscountPolicy;
-import spring.springcorebasic.discount.FixDiscountPolicy;
 import spring.springcorebasic.member.Member;
 import spring.springcorebasic.member.MemberRepository;
-import spring.springcorebasic.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService{
 
+    /*
+    //private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    private final DiscountPolicy discountPolicy = new RateDiscountPoicy();
+    // 위의 방식은 DIP, OCP를 위반한다. 구체에 의존하지 않고 추상화에 의존해야 한다.
+    */
+
+    //아래는 DIP를 충족하기 위한 변경된 코드이다.
+    private final DiscountPolicy discountPolicy;
+
     // 회원의 등급을 확인하기 위함 => 할인정책 적용을 위해
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    private final MemberRepository memberRepository;
+
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.discountPolicy = discountPolicy;
+        this.memberRepository = memberRepository;
+    }
+
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
         Member member = memberRepository.findById(memberId); // 사용자의 등급을 통해 할인정책을 달리 하기 위함
