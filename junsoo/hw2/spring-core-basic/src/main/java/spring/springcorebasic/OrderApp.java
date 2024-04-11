9e34056af956bc7474621fd9d410a68fdb886f18
ijunsoo <159103870+ijunsoo@users.noreply.github.com>
2024-04-11 16:18:22 +0900

    section 2-3-1

--- junsoo/hw2/spring-core-basic/src/main/java/spring/springcorebasic/OrderApp.java
+++ junsoo/hw2/spring-core-basic/src/main/java/spring/springcorebasic/OrderApp.java
@@ -1,5 +1,7 @@
 package spring.springcorebasic;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import spring.springcorebasic.member.Grade;
 import spring.springcorebasic.member.Member;
 import spring.springcorebasic.member.MemberService;
@@ -10,9 +12,12 @@ import spring.springcorebasic.order.OrderServiceImpl;
 
 public class OrderApp {
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
-        OrderService orderService = new OrderServiceImpl();
-
+        ApplicationContext applicationContext =new
+                AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService =
+                applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService =
+                applicationContext.getBean("orderService",OrderService.class);
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
         memberService.join(member);
