3481ede420b3d8c300f270c8aac7e095fc9c2254
ujii <wcm9763@naver.com>
2024-05-01 13:13:07 +0900

    📦️ 3-2 : lombok 설정과 라이브러리 추가
    build.gradle 파일에 lombok 설정과 lombok 라이브러리 추가

--- jihyun/hw3/spring-core-basic/src/main/java/spring/springcorebasic/order/OrderServiceImpl.java
+++ jihyun/hw3/spring-core-basic/src/main/java/spring/springcorebasic/order/OrderServiceImpl.java
@@ -15,7 +15,8 @@ public class OrderServiceImpl implements OrderService{
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
-    @Autowired
+    // @Autowired
+    // 생성자가 하나이므로 @Autowired 생략 가능
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
