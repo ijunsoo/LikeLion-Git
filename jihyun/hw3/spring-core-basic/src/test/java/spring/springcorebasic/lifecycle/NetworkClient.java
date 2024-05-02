0fceb6d47f9c86687def1ddb659add59e1ffaef9
ujii <wcm9763@naver.com>
2024-05-02 12:57:52 +0900

    ✅ 3-3 : NetworkClient 수정
    @PostConstruct, @PreDestroy 애노테이션을 이용해 빈 등록

--- jihyun/hw3/spring-core-basic/src/test/java/spring/springcorebasic/lifecycle/NetworkClient.java
+++ jihyun/hw3/spring-core-basic/src/test/java/spring/springcorebasic/lifecycle/NetworkClient.java
@@ -1,12 +1,17 @@
 package spring.springcorebasic.lifecycle;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+
 public class NetworkClient {
     private String url;
 
     public NetworkClient()  {
         System.out.println("생성자 호출, url = " + url);
-        connect();
-        call("초기화 연결 메시지");
+
+//        여기서 호출하면 url = null
+//        connect();
+//        call("초기화 연결 메시지");
     }
 
     public void setUrl(String url) {
@@ -25,5 +30,22 @@ public class NetworkClient {
     // 서비스 종료시 호출
     public void disconnect() {
         System.out.println("close : " + url);
+    }
+
+
+
+    // 빈 등록을 초기화하는 메소드
+    @PostConstruct
+    public void init() {
+        System.out.println("NetworkClient.init");
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    // 빈 등록 소명 메소드
+    @PreDestroy
+    public void close() {
+        System.out.println("NetworkClient.close");
+        disconnect();
     }
 }
