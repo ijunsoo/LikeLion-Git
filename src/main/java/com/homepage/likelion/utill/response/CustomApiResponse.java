e93fc3387f985811555c264a256373f315143b2e
ijunsoo <dlwnstni74@gmail.com>
2024-05-08 22:57:16 +0900

    :white_check_mark:section 4-2 customapiresponse작성

---
+++ src/main/java/com/homepage/likelion/utill/response/CustomApiResponse.java
@@ -0,0 +1,25 @@
+package com.homepage.likelion.utill.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CustomApiResponse<T> {
+    private int status;
+    private T data;
+
+    private String message;
+
+    public static <T> CustomApiResponse<T> createSuccess(int status, T data, String message){
+        return new CustomApiResponse<>(status, data, message);
+    }
+    public static <T> CustomApiResponse<T> createFailWithout(int status,String message){
+        return new CustomApiResponse<>(status, null,message);
+    }
+
+}
