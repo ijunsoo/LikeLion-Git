9859987c547f37cd63384631c4dda9bdedff77ef
ijunsoo <dlwnstni74@gmail.com>
2024-05-09 14:51:41 +0900

    :white_check_mark: section 4-4 BaseEntity 작성

--- src/main/java/com/homepage/likelion/test/domain/User.java
+++ src/main/java/com/homepage/likelion/test/domain/User.java
@@ -1,5 +1,6 @@
 package com.homepage.likelion.test.domain;
 
+import com.homepage.likelion.utill.entity.BaseEntity;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -12,7 +13,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class User {
+public class User extends BaseEntity {
     @Id@GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="id")
     private Long id;
